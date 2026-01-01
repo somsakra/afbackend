f7a6dbda65a2a37c047bb9eb98d591e2dcf6b13d
Somsak Rakjoi <somsak.rak@ttbbank.com>
2026-01-01 11:37:48 +0700

    add prisma db

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-import { db } from "./db";
+import { prisma } from "./lib/prisma.js";
+import express, { Request, Response, Application } from 'express';
 
-console.log("Database Connection:", db);
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/', async (req: Request, res: Response) => {
+    console.log(req.body);
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password
+        }
+    });
+    res.json(user);
+});
+   
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
